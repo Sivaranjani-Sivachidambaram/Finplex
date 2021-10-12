fc181a7d4ed535c1d1c349f8b29328de57dab9b3
Sivaranjani-Sivachidambaram <81016172+Sivaranjani-Sivachidambaram@users.noreply.github.com>
2021-10-12 13:02:25 +0530

    Build

---
+++ index.web.ts
@@ -0,0 +1,10 @@
+import {AppRegistry} from 'react-native'
+import {expo as appName} from './app.json'
+import App from './App'
+
+AppRegistry.registerComponent(appName.name, () => App)
+
+AppRegistry.runApplication(appName.name, {
+  initialProps: {},
+  rootTag: document.getElementById('app-root'),
+})
